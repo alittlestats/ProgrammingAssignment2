fcc39a24dbebb4193e212ef302d7d01b5bdd60c2
alittlestats <alittlestats@gmail.com>
2015-08-22 09:49:58 -0400

    my version of the cachematrix file

---
+++ cachemean.R
@@ -0,0 +1,32 @@
+##creates a special "vector"
+##set the value of the vector
+##get the value of the vector
+##set the value of the mean
+##get the value of the mean
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+
+##calculates the mean of the special "vector" 
+##created with the above function
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
+}
